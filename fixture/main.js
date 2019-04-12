e06c73db976213a9e50b6675614ad40896d398df
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-04-12 18:59:40 +0900

    Fix sdp transform

--- fixture/main.js
+++ fixture/main.js
@@ -54,27 +54,53 @@ function paramsToAnswerSDP(offerSdp, { iceParams, iceCandidates }) {
 
   let sdpLines = offerSdp.split('\r\n');
 
+  const mLine = sdpLines.find(line => line.startsWith('m='));
+  const [mMain, , , mRtpmapVal] = mLine.split(' ');
+  const fixedMLine = [mMain, '7', 'RTP/SAVPF', mRtpmapVal].join(' ');
+
   sdpLines = sdpLines.map(line => {
-    if (line.startsWith('a=fingerprint')) {
-      // tslint:disable-next-line:max-line-length
-      return 'a=fingerprint:sha-512 10:13:09:9F:88:F4:A6:D0:18:F3:AA:F5:01:9A:E6:8A:29:FF:9E:E1:40:56:F3:97:C6:46:6A:17:FA:06:83:65:E6:85:FE:A6:30:20:48:10:EA:73:74:1A:9A:D3:66:63:01:82:F7:FA:00:EA:77:27:2B:1B:9B:C6:30:25:E5:06';
-    }
     if (line.startsWith('a=send')) {
       return 'a=recvonly';
     }
     if (line.startsWith('a=setup')) {
       return 'a=setup:active';
     }
-    if (line.startsWith('a=candidate') || line.startsWith('a=msid') || line.startsWith('a=ssrc')) {
+    if (line.startsWith('m=')) {
+      return fixedMLine;
+    }
+    if (line.startsWith('c=')) {
+      return 'c=IN IP4 127.0.0.1';
+    }
+    if (line.startsWith('a=rtpmap')) {
+      if (line.startsWith(`a=rtpmap:${mRtpmapVal}`)) {
+        return line;
+      }
+        return '';
+
+    }
+    if (
+      line.startsWith('a=candidate') ||
+      line.startsWith('a=ice-') ||
+      line.startsWith('a=msid') ||
+      line.startsWith('a=extmap') ||
+      line.startsWith('a=fmtp') ||
+      line.startsWith('a=rtcp') ||
+      line.startsWith('a=fingerprint') ||
+      line.startsWith('a=ssrc')
+    ) {
       return '';
     }
-
     return line;
   }).filter(Boolean);
 
   // pretend ICE-Lite server
-  // TODO: why this can't be added...?
-  // sdpLines.splice(4, 0, 'a=ice-lite');
+  sdpLines.splice(4, 0,
+    // TODO: enable this ends up w/ missing USE-CANDIDATE...
+    // 'a=ice-lite',
+    // tslint:disable-next-line:max-line-length
+    'a=fingerprint:sha-512 12:DC:40:86:73:D6:F0:29:60:08:92:E4:14:52:E7:58:12:B5:A5:A5:6C:75:29:34:F9:EF:72:8B:06:C4:D8:B2:D0:A8:F9:CD:42:CC:05:B2:57:76:19:82:4E:14:90:84:68:96:64:D2:C8:76:55:CA:C5:3C:82:0E:54:14:85:60',
+    'a=msid-semantic: WMS *',
+  );
 
   sdpLines.push(
     `a=ice-ufrag:${usernameFragment}`,
@@ -91,6 +117,8 @@ function paramsToAnswerSDP(offerSdp, { iceParams, iceCandidates }) {
   }
   sdpLines.push(
     'a=end-of-candidates',
+    'a=rtcp-mux',
+    'a=rtcp-rsize',
     '',
   );
 
